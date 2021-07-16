40b5e46a5c7cf752404c50b67dc239e60d45a74f
gereldan <gerel@bk.ru>
2021-07-16 15:22:20 +0500

    commit one

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
+
 df = pd.read_csv('keywords (1).csv', sep=';')
-df.info()
-print(len(df))
+
 for i in range(len(df)):
     df.loc[i, "'Keyword'"] = " ".join([s.replace('[', '').replace(']', '')
                                        for s in df.loc[i, "'Keyword'"].split() if not s.startswith('-')])
-print(df["'Keyword'"].head(5))
